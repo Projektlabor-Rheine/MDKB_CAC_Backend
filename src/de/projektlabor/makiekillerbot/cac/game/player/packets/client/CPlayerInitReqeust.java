6ecb97b7ed5f372bf4fa866a4079df664ea63865
Noah Albers <albers-noah@pm.me>
2021-05-11 10:13:18 +0200

    Added missing description to packet

--- src/de/projektlabor/makiekillerbot/cac/game/player/packets/client/CPlayerInitReqeust.java
+++ src/de/projektlabor/makiekillerbot/cac/game/player/packets/client/CPlayerInitReqeust.java
@@ -5,6 +5,12 @@ import org.json.JSONObject;
 import de.projektlabor.makiekillerbot.cac.connection.packets.IPacketClient;
 import de.projektlabor.makiekillerbot.cac.game.player.Player;
 
+/**
+ * Request by the client to resend his init-packet as it may got lost on the way
+ * @author Noah
+ *
+ */
+
 public class CPlayerInitReqeust implements IPacketClient<Player>{	
 	@Override
 	public void readPacketData(JSONObject packet) throws Exception {}
